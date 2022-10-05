57676272a99e23247b9d567942f2434eaabc1ca4
Mensah <Mensah@DESKTOP-29Q93C5>
2022-10-05 22:09:48 +0000

    wip

--- Gamification.UI/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Gamification.UI/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -4,13 +4,10 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Text;
-using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 
 namespace Gamification.UI.Areas.Identity.Pages.Account
@@ -79,26 +76,28 @@ namespace Gamification.UI.Areas.Identity.Pages.Account
 				{
 					_logger.LogInformation("User created a new account with password.");
 
-					var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-					code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-					var callbackUrl = Url.Page(
-							"/Account/ConfirmEmail",
-							pageHandler: null,
-							values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
-							protocol: Request.Scheme);
+					//var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+					//code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+					//var callbackUrl = Url.Page(
+					//		"/Account/ConfirmEmail",
+					//		pageHandler: null,
+					//		values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
+					//		protocol: Request.Scheme);
 
-					await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-							$"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+					//await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+					//		$"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-					if (_userManager.Options.SignIn.RequireConfirmedAccount)
-					{
-						return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-					}
-					else
-					{
-						await _signInManager.SignInAsync(user, isPersistent: false);
-						return LocalRedirect(returnUrl);
-					}
+					//if (_userManager.Options.SignIn.RequireConfirmedAccount)
+					//{
+					//	return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+					//}
+					//else
+					//{
+					//	await _signInManager.SignInAsync(user, isPersistent: false);
+					//	return LocalRedirect(returnUrl);
+					//}
+					await _signInManager.SignInAsync(user, isPersistent: true);
+					return LocalRedirect(returnUrl);
 				}
 				foreach (var error in result.Errors)
 				{
--- Gamification.UI/Controllers/HomeController.cs
+++ Gamification.UI/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿using Gamification.UI.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 
 namespace Gamification.UI.Controllers
 {
+	[Authorize]
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
