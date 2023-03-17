e8c3066e0b8f53e33f760af1f97d148dbab970db
Ernest Kyei Nkrumah <Mishrule1@gmail.com>
2023-03-17 22:05:41 +0000

    wip

--- Gamification.UI/Controllers/HomeController.cs
+++ Gamification.UI/Controllers/HomeController.cs
@@ -45,121 +45,122 @@ namespace Gamification.UI.Controllers
 
 	public String GetUrl(int clientId, String userId, String applicationServer)
 	{
-		
-	  return	$"https://{applicationServer.Trim()}/sap/opu/odata/sap/ZUCC_GBM_SRV/MM_FSet(Id=2,User='{userId.ToUpper().Trim()}')?$format=json&sap-client={clientId}";
+				return $"https://{applicationServer.Trim()}/sap/opu/odata/sap/ZUCC_GBM_GM_SRV/MM_FSet(Id=2,User='{userId.ToUpper().Trim()}')?$format=json&sap-client={clientId}";
+			//return $"https://{applicationServer.Trim()}/sap/opu/odata/sap/ZUCC_GBM_SRV/MM_FSet(Id=2,User='{userId.ToUpper().Trim()}')?$format=json&sap-client={clientId}";
+	}	
+	
+	public String GetUrl(int clientId, String userId, String applicationServer, string caseStudy = "MM")
+	{
+				return $"https://{applicationServer.Trim()}/sap/opu/odata/sap/ZUCC_GBM_GM_SRV/{caseStudy}_FSet(Id=2,User='{userId.ToUpper().Trim()}')?$format=json&sap-client={clientId}";
+			//return $"https://{applicationServer.Trim()}/sap/opu/odata/sap/ZUCC_GBM_SRV/MM_FSet(Id=2,User='{userId.ToUpper().Trim()}')?$format=json&sap-client={clientId}";
 	}
 
 
-	public async Task<ActionResult> Index(/*int clientId, String userId, String applicationServer*/)
+	public async Task<ActionResult> Index()
 	{
 		try
 		{
 			var userInfo = new ApplicationUser();
 
-				var userList = _db.ApplicationUsers.ToList().Where(q=>q.UserId == HttpContext.User.Identity.Name.ToUpper());
-				foreach (var data in userList)
+			var userList = _db.ApplicationUsers.ToList().Where(q=>q.UserId == HttpContext.User.Identity.Name.ToUpper());
+			foreach (var data in userList)
+			{
+				userInfo =	new ApplicationUser()
 				{
-				   userInfo =	new ApplicationUser()
-					{
-						ApplicationServer = data.ApplicationServer,
-						ClientId = data.ClientId,
-						UserId = data.UserId
-					};
-				}
-				var userName = "eadeborna";
-				var passwd = "Gamification123";
+					ApplicationServer = data.ApplicationServer,
+					ClientId = data.ClientId,
+					UserId = data.UserId
+				};
+			}
+			var userName = "eadeborna";
+			var passwd = "Gamification123";
 
-				var url = GetUrl(userInfo.ClientId, userInfo.UserId, userInfo.ApplicationServer);
+			var url = GetUrl(userInfo.ClientId, userInfo.UserId, userInfo.ApplicationServer);
 
-				// use this handler to allow untrusted SSL Certificates
-				var handler = new HttpClientHandler();
-				handler.ClientCertificateOptions = ClientCertificateOption.Manual;
-				handler.ServerCertificateCustomValidationCallback =
-					(httpRequestMessage, cert, cetChain, policyErrors) =>
-					{
-						return true;
-					};
-
-				using var client = new HttpClient(handler);
-
-				var authToken = Encoding.ASCII.GetBytes($"{userName}:{passwd}");
-				client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
-					Convert.ToBase64String(authToken));
-
-				var result = await client.GetAsync(url);
-
-				var content = await result.Content.ReadAsStringAsync();
-
-				//dynamic data = JsonConvert.DeserializeObject<dynamic>(content);
-
-				// Access the properties like this:
-				//int id = data.d.Id;
-				//string user = data.d.User;
-				//string step1 = data.d.Step1;
-				//string step2 = data.d.Step2;
-				//string step3 = data.d.Step3;
-				//string step4 = data.d.Step4;
-				//string step5 = data.d.Step5;
-				//string step6 = data.d.Step6;
-				//string step7 = data.d.Step7;
-				//string step8 = data.d.Step8;
-				//string step9 = data.d.Step9;
-				//string step10 = data.d.Step10;
-				//string step11 = data.d.Step11;
-				//string step12 = data.d.Step12;
-				//string step13 = data.d.Step13;
-				//string fulfillmentMandatory = data.d.FulfillmentMandatory;
-				//string fulfillmentAll = data.d.FulfillmentAll;
-
-
-
-				//dynamic json = JsonConvert.DeserializeObject(content);
-				//ParseJson(json);
-				var dictionaryList = new List<DictionaryModel>();
-				var jObject = JObject.Parse(content);
-				var jToken = jObject["d"];
-
-
-				var PiontsDictionary = new Dictionary<string, int>()
+			// use this handler to allow untrusted SSL Certificates
+			var handler = new HttpClientHandler();
+			handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+			handler.ServerCertificateCustomValidationCallback =
+				(httpRequestMessage, cert, cetChain, policyErrors) =>
 				{
-					{"@08@", 10},
-					{"@09@", 8},
-					{"@0A@", 7 },
-					{"", 0}
+					return true;
 				};
 
-				foreach (var property in jToken.Children<JProperty>())
+			using var client = new HttpClient(handler);
+
+			var authToken = Encoding.ASCII.GetBytes($"{userName}:{passwd}");
+			client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+				Convert.ToBase64String(authToken));
+
+			var result = await client.GetAsync(url);
+
+			var content = await result.Content.ReadAsStringAsync();
+
+				
+			var dictionaryList = new List<DictionaryModel>();
+			var jObject = JObject.Parse(content);
+			var jToken = jObject["d"];
+
+
+			var PiontsDictionary = new Dictionary<string, int>()
+			{
+				{"@08@", 10},
+				{"@09@", 8},
+				{"@0A@", 7 },
+				{"", 0}
+			};
+
+			foreach (var property in jToken.Children<JProperty>())
+			{
+				var dictionaryModel = new DictionaryModel
 				{
-					var dictionaryModel = new DictionaryModel
-					{
-						Key = property.Name,
-						Value = property.Value.ToString()
-					};
-					dictionaryList.Add(dictionaryModel);
+					Key = property.Name,
+					Value = property.Value.ToString()
+				};
+				dictionaryList.Add(dictionaryModel);
+			}
+
+			// Get points for that specific learn id
+			int point = 0;
+			int level = 0;
+			string fulfill = "";
+			List<int> Points = new List<int>();
+			foreach (var item in dictionaryList)
+			{
+				//if (item.Key.Contains("Step"))
+				//{
+				//	point = PiontsDictionary[item.Value];
+				//	Points.Add(point);
+				//	if (point != 0)
+				//		level++;
+				//}
+
+				if (item.Key.Equals("FulfillmentAll"))
+				{
+					fulfill = item.Value;
 				}
 
-				// Get points for that specific learn id
-				int point = 0;
-				int level = 0;
-				List<int> Points = new List<int>();
-				foreach (var item in dictionaryList)
+				if (item.Key.Equals("Points"))
 				{
-					if (item.Key.Contains("Step"))
-					{
-						point = PiontsDictionary[item.Value];
-						Points.Add(point);
-						if (point != 0)
-							level++;
-					}
+					point = int.Parse(item.Value);
 				}
+				if (item.Key.Equals("Level"))
+				{
+					level = int.Parse(item.Value);
+				}
+			}
 
-				ViewBag.Point = Points.Sum();
-				ViewBag.Levels = level;
+			// ful
+			string aa = fulfill.Remove(fulfill.IndexOf('%'));
+			ViewBag.Fulfillment =  int.Parse(aa);
+			//ViewBag.Point = Points.Sum();
+			ViewBag.Point = point;
+			ViewBag.Levels = level;
 
 
 				
-				return View();
-			}
+			return View();
+		}
 		catch (Exception e)
 		{
 			Console.WriteLine(e);
@@ -168,6 +169,114 @@ namespace Gamification.UI.Controllers
 		
 
 			
+	}
+	public async Task<ActionResult> CaseStudy(string caseStudy)
+	{
+		try
+		{
+			var userInfo = new ApplicationUser();
+
+			var userList = _db.ApplicationUsers.ToList().Where(q => q.UserId == HttpContext.User.Identity.Name.ToUpper());
+			foreach (var data in userList)
+			{
+				userInfo = new ApplicationUser()
+				{
+					ApplicationServer = data.ApplicationServer,
+					ClientId = data.ClientId,
+					UserId = data.UserId
+				};
+			}
+			var userName = "eadeborna";
+			var passwd = "Gamification123";
+
+			var url = GetUrl(userInfo.ClientId, userInfo.UserId, userInfo.ApplicationServer, caseStudy);
+
+			// use this handler to allow untrusted SSL Certificates
+			var handler = new HttpClientHandler();
+			handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+			handler.ServerCertificateCustomValidationCallback =
+				(httpRequestMessage, cert, cetChain, policyErrors) =>
+				{
+					return true;
+				};
+
+			using var client = new HttpClient(handler);
+
+			var authToken = Encoding.ASCII.GetBytes($"{userName}:{passwd}");
+			client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+				Convert.ToBase64String(authToken));
+
+			var result = await client.GetAsync(url);
+
+			var content = await result.Content.ReadAsStringAsync();
+
+
+			var dictionaryList = new List<DictionaryModel>();
+			var jObject = JObject.Parse(content);
+			var jToken = jObject["d"];
+
+
+			var PiontsDictionary = new Dictionary<string, int>()
+			{
+				{"@08@", 10},
+				{"@09@", 8},
+				{"@0A@", 7 },
+				{"", 0}
+			};
+
+			foreach (var property in jToken.Children<JProperty>())
+			{
+				var dictionaryModel = new DictionaryModel
+				{
+					Key = property.Name,
+					Value = property.Value.ToString()
+				};
+				dictionaryList.Add(dictionaryModel);
+			}
+
+			// Get points for that specific learn id
+			int point = 0;
+			int level = 0;
+			string fulfill = "";
+			List<int> Points = new List<int>();
+			foreach (var item in dictionaryList)
+			{
+				
+
+				if (item.Key.Equals("FulfillmentAll"))
+				{
+					fulfill = item.Value;
+				}
+
+				if (item.Key.Equals("Points"))
+				{
+					point = int.Parse(item.Value);
+				}
+				if (item.Key.Equals("Level"))
+				{
+					level = int.Parse(item.Value);
+				}
+			}
+
+			// ful
+			string aa = fulfill.Remove(fulfill.IndexOf('%'));
+			ViewBag.Fulfillment = int.Parse(aa);
+			//ViewBag.Point = Points.Sum();
+			ViewBag.Point = point;
+			ViewBag.Levels = level;
+
+
+
+			return View();
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine(e);
+			throw;
+		}
+
+
+
 	}
 	public static List<DictionaryModel> ParseJson(string jsonString)
 	{
@@ -187,76 +296,76 @@ namespace Gamification.UI.Controllers
 
 		return dictionaryList;
 	}
-		public class DictionaryModel
+	public class DictionaryModel
 	{
 		public string Key { get; set; }
 		public string Value { get; set; }
 	}
 
-		[HttpPost]
+	[HttpPost]
 	public async Task<IActionResult> Index(string username)
 	{
 
-	  var p = 0;
-	  var b = 0;
-	  var c = 0;
+		var p = 0;
+		var b = 0;
+		var c = 0;
 
-	  var points = 0;
-	  var data = await _tasksServices.GetResponsePoint(username);
-	  foreach (var item in data)
-	  {
-		points += item.Score;
-	  }
-	  if (points > 30)
-	  {
-		p = points;
-		b = 2;
-		c = 1;
-	  }
+		var points = 0;
+		var data = await _tasksServices.GetResponsePoint(username);
+		foreach (var item in data)
+		{
+			points += item.Score;
+		}
+		if (points > 30)
+		{
+			p = points;
+			b = 2;
+			c = 1;
+		}
 
-	  else if (points >= 15 && points < 30)
-	  {
-		p = points;
-		b = 1;
-	  }
-	  else
-	  {
-		p = points;
-	  }
+		else if (points >= 15 && points < 30)
+		{
+			p = points;
+			b = 1;
+		}
+		else
+		{
+			p = points;
+		}
 
-	  var dataa = new
-	  {
-		p,
-		b,
-		c
-	  };
+		var dataa = new
+		{
+			p,
+			b,
+			c
+		};
 
-	  return Ok(dataa);
+		return Ok(dataa);
 	}
 
 	public async Task<IActionResult> LeaderBoard()
 	{
 		var data = await _tasksServices.GetLeaders();
-	  return View(data);
+		return View(data);
 	}
 
 	public async Task<IActionResult> Tasks()
 	{
-	  var data = await _tasksServices.GetTasks();
-	  return View(data);
+		var data = await _tasksServices.GetTasks();
+		return View(data);
 	}
 	[HttpPost]
 	public async Task<IActionResult> TaskResponse(TasksResponse taskResponse)
 	{
-	  var data = await _tasksServices.CreateResponse(taskResponse);
-	  //await Index(taskResponse.RespondantName);
-	  return Ok(data);
+		var data = await _tasksServices.CreateResponse(taskResponse);
+		//await Index(taskResponse.RespondantName);
+		return Ok(data);
 	}
 
 	public IActionResult TaskResponse()
 	{
 
-	  return View();
+		return View();
 	}
 
 	public IActionResult Badges()
